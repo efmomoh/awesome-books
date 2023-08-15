5618e7bbcba9bbecd9d28c09120ec618270b5f10
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-08-15 14:44:42 +0100

    added the booklisting

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const awesomeBooks = [];
 
 // Function to add a book
 function addBook() {
-  const bookTitle = document.getElementById('bookTitle').value;
-  const authorName = document.getElementById('authorName').value;
-  const errorMessage = document.querySelector('.feedback');
+  const bookTitle = document.getElementById("bookTitle").value;
+  const authorName = document.getElementById("authorName").value;
+  const errorMessage = document.querySelector(".feedback");
 
-  if (bookTitle.trim() === '' || authorName.trim() === '') {
+  if (bookTitle.trim() === "" || authorName.trim() === "") {
     errorMessage.textContent = '❌ Add both "Title" & "Author"!';
     return;
   }
@@ -15,8 +15,8 @@ function addBook() {
   const book = { title: bookTitle, author: authorName };
   awesomeBooks.push(book);
 
-  document.getElementById('bookTitle').value = '';
-  document.getElementById('authorName').value = '';
+  document.getElementById("bookTitle").value = "";
+  document.getElementById("authorName").value = "";
   /* eslint-disable */
   updateBookList();
   /* eslint-enable */
@@ -32,21 +32,21 @@ function removeBook(index) {
 
 // Function to update the book list on the page
 function updateBookList() {
-  const bookListing = document.getElementById('bookList');
-  bookListing.innerHTML = '';
+  const bookListing = document.getElementById("bookList");
+  bookListing.innerHTML = "";
 
-  // for (let i = 0; i < awesomeBooks.length; i += 1) {
-  //   const book = awesomeBooks[i];
-  //   const listBookItem = document.createElement('li');
-  //   listBookItem.classList.add('lists');
-  //   listBookItem.textContent = `Title: ${book.title}, Author: ${book.author}`;
+  for (let i = 0; i < awesomeBooks.length; i += 1) {
+    const book = awesomeBooks[i];
+    const listBookItem = document.createElement("li");
+    listBookItem.classList.add("lists");
+    listBookItem.textContent = `Title: ${book.title}, Author: ${book.author}`;
 
-  //   const removeButton = document.createElement('button');
-  //   removeButton.textContent = 'Remove';
-  //   removeButton.classList.add('remove-btn');
-  //   removeButton.addEventListener('click', () => {
-  //     removeBook(i);
-  //   });
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.classList.add("remove-btn");
+    removeButton.addEventListener("click", () => {
+      removeBook(i);
+    });
 
     listBookItem.appendChild(removeButton);
     bookListing.appendChild(listBookItem);
@@ -54,12 +54,12 @@ function updateBookList() {
 }
 
 // Add event listeners to buttons
-document.getElementById('add-btn').addEventListener('click', addBook);
+document.getElementById("add-btn").addEventListener("click", addBook);
 
 // Local storage / save user data locally
-const locateLocalBooks = document.querySelector('.form');
-const titleName = document.querySelector('.bookHeading');
-const nameOfAuthor = document.querySelector('.orator');
+const locateLocalBooks = document.querySelector(".form");
+const titleName = document.querySelector(".bookHeading");
+const nameOfAuthor = document.querySelector(".orator");
 
 // collect form data
 
@@ -73,15 +73,15 @@ function getBookData() {
 
 // Add an event listener to the local form storage area
 
-locateLocalBooks.addEventListener('change', () => {
+locateLocalBooks.addEventListener("change", () => {
   const bookData = getBookData();
-  localStorage.setItem('bookData', JSON.stringify(bookData));
+  localStorage.setItem("bookData", JSON.stringify(bookData));
 });
 
 // Add an eevnt listener to the page/window to save input data when user loads the page
 
-window.addEventListener('load', () => {
-  const data = JSON.parse(localStorage.getItem('bookData'));
+window.addEventListener("load", () => {
+  const data = JSON.parse(localStorage.getItem("bookData"));
   if (data) {
     titleName.value = data.titleName;
     nameOfAuthor.value = data.nameOfAuthor;
